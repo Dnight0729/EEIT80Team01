1bdb44545a7f4313ecae1ec8b14aff19a0c94d8d
lou <jhih.lou@gmail.com>
2015-09-30 10:29:58 +0800

    add exception Servlet and jsp
    add exception Servlet and jsp

---
+++ EEIT80Team01/WebContent/exception/exceptionPage.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<%@include file="/include/include" %>
+<title>Don't worry! it will be OK!</title>
+</head>
+<body>
+<header>
+<%@include file="/include/header" %>
+</header>
+<article>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>Servlet Name</th>
+				<th>Exception Name</th>
+				<th>Request URI</th>
+				<th>Exception Message</th>
+			</tr>	
+		</thead>
+		<tbody>
+			<tr>
+				<td>{servletName}</td>
+				<td>{exceptionName}</td>
+				<td>{requestUri}</td>
+				<td>{exceptionMsg}</td>
+			</tr>
+		</tbody>
+	</table>
+</article>
+</body>
+</html>
---
+++ EEIT80Team01/src/exception/ExceptionHandler.java
@@ -0,0 +1,49 @@
+package exception;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("exception/ExceptionHandler")
+public class ExceptionHandler extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processError(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processError(request, response);
+	}
+	private void processError(HttpServletRequest request,
+			HttpServletResponse response)throws ServletException,IOException{
+			Throwable throwable = 
+					(Throwable)request.getAttribute("javax.servlet.error.exception");
+			String exceptionName = throwable.getClass().getName();
+			String exceptionMsg = throwable.getMessage();
+			Integer statusCode = 
+					(Integer)request.getAttribute("javax.servlet.error.status_code");
+			String servletName = 
+					(String)request.getAttribute("javax.servlet.error.servlet_name");
+			String requestUri = 
+					(String)request.getAttribute("javax.servlet.error.request_uri");
+			if(servletName==null){
+				servletName = "不知名的程式";
+			}
+			if(requestUri==null){
+				requestUri = "不知名的請求";
+			}
+			request.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			request.setAttribute("servletName",servletName);
+			request.setAttribute("requestUri",requestUri);
+			request.setAttribute("statusCode",statusCode);
+			request.setAttribute("exceptonName",exceptionName);
+			request.setAttribute("exceptionMsg",exceptionMsg);
+			request.getRequestDispatcher("exceptonPage.jsp").forward(request, response);
+	}
+}
